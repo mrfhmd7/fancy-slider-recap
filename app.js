670d7818c43a9236ffbff3eee61e30c6c70a894d
mrfhmd7 <mrf.hmd7@gmail.com>
2023-12-09 20:42:22 +0600

    fix image problem

--- app.js
+++ app.js
@@ -32,8 +32,8 @@ const getImages = (query) => {
   fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
     .then(response => response.json())
     .then(data => {
-      console.log(data);
-      showImages(data.hitS)
+      // console.log(data.hits);
+      showImages(data.hits)
     })
     .catch(err => console.log(err))
 }
